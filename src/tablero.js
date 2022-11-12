10b9e9a678cd77069608574c29044508dc828d69
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-11-12 15:22:52 -0300

    test

--- src/tablero.js
+++ src/tablero.js
@@ -1,4 +1,15 @@
 function Tablero(){
     this.estado="vacio";
+    this.linea=new Array(4);
+    this.insertarPaquete(){
+        var i;
+        for(i=0;i<4;i++){
+            if(this.linea[i]==null){
+                this.linea[i]=1;
+                break;
+
+            }
+        }
+    }
 }
 module.exports=Tablero;
--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -1,7 +1,14 @@
 
 const Tablero= require('../src/tablero');
 test("tablero vacio",()=>{
-    const tablero=new Tablero()
+    const tablero=new Tablero();
     expect(tablero.estado).toBe("vacio");
 
 })
+test("insertar paquete",()=>{
+    const tablero=new Tablero();
+    tablero.insertarPaquete();
+    expect(tablero[0]).toBe(1);
+})
+
+
