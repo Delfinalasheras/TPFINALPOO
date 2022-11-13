050626e858258f7ecc601ba8fdf9980aa9706020
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-11-13 16:55:34 -0300

    test

--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -1,6 +1,6 @@
 const Local = require("../src/Local");
 const Tablero= require('../src/tablero');
-const Tablero= require('../src/Paquete');
+const Paquete= require('../src/Paquete');
 
 test("tablero vacio",()=>{
     const tablero=new Tablero();
@@ -10,9 +10,9 @@ test("tablero vacio",()=>{
 test("insertar paquete",()=>{
     const local=new Local("A");
     local.insertarPaquete(3);
-    expect(local.paquetesLocal[0]).toBe(paquete);
-    expect(local.paquetesLocal[1]).toBe(paquete);
-    expect(local.paquetesLocal[2]).toBe(paquete);
+    expect(local.paquetesLocal[0]).toBe(Paquete);
+    expect(local.paquetesLocal[1]).toBe(Paquete);
+    expect(local.paquetesLocal[2]).toBe(Paquete);
 
 
 })
