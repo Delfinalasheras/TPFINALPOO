976dd01069e0ceacacfe0483cbb00748b42d8ee6
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-11-13 17:05:28 -0300

    test

--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -8,7 +8,7 @@ test("tablero vacio",()=>{
     expect(tablero.estado).toBe("vacio");
 
 })
-test("insertar paquete",()=>{
+test("insertar paquete en local",()=>{
     const local=new Local("A");
     local.insertarPaquete(3);
     expect(local.paquetesLocal[0].getPaquete()).toBe("Paquete");
@@ -17,7 +17,13 @@ test("insertar paquete",()=>{
 
 
 })
-test("insertar otro Paquete",()=>{
+test("Mover Paquetes",()=>{
+    const tablero=new Tablero();
+    const local=new Local("A");
+    local.insertarPaquete(3);
+    tablero.moverPaquetes(3);
+    expect(tablero[2]).toBe(3);
+    
     
 
 
