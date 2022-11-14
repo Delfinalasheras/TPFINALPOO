2b7db6213daa272affcb882ddf162e53e23bdf79
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-11-13 21:13:02 -0300

    test

--- src/tablero.js
+++ src/tablero.js
@@ -6,16 +6,13 @@ const Distribucion = require("./centros/Distribucion");
 const Paquete = require("./Paquete");
 function Tablero(){
     this.estado=[];
-    this.facturacion=new Facturacion();
-    this.calidad=new Calidad();
-    this.distribucion=new Distribucion();
     this.crearTablero=function(nombreLocal,cantidad){
         const local=new Local(nombreLocal);
         local.insertarPaquete(cantidad);
         this.estado[0]=local.paquetesLocal;
-        this.estado[1]=Array(facturacion.maximoPaquetes);
-        this.estado[3]=Array(calidad.maximoPaquetes);
-        this.estado[4]=Array(distribucion.maximoPaquetes);
+        this.estado[1]=Array(Facturacion.maximoPaquetes);
+        this.estado[3]=Array(Calidad.maximoPaquetes);
+        this.estado[4]=Array(Distribucion.maximoPaquetes);
 
     }
     this.moverPaquete=function(){
