753545df14395c62b32a51f4c9bfb408c3f88727
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-11-13 21:11:20 -0300

    test

--- src/tablero.js
+++ src/tablero.js
@@ -13,9 +13,9 @@ function Tablero(){
         const local=new Local(nombreLocal);
         local.insertarPaquete(cantidad);
         this.estado[0]=local.paquetesLocal;
-        //this.estado[1]=Array(facturacion.maximoPaquetes);
-        //this.estado[3]=Array(calidad.maximoPaquetes);
-        //this.estado[4]=Array(distribucion.maximoPaquetes);
+        this.estado[1]=Array(facturacion.maximoPaquetes);
+        this.estado[3]=Array(calidad.maximoPaquetes);
+        this.estado[4]=Array(distribucion.maximoPaquetes);
 
     }
     this.moverPaquete=function(){
